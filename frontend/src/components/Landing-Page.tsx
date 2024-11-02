9bfa0a5b13195d7b518d07525b0d2049425512b9
LoicCh <loic.chayla@gmail.com>
2024-11-02 17:52:59 -0400

    landing page

--- frontend/src/components/Landing-Page.tsx
+++ frontend/src/components/Landing-Page.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import Navbar from './ui/navbar';
-import firebase from 'firebase/compat/app';
-import Hero12 from './ui/hero-section';
+import React from "react";
+import Navbar from "./ui/navbar";
+import firebase from "firebase/compat/app";
+import Hero12 from "./ui/hero-section";
 
 interface LandingPageProps {
   setUser: React.Dispatch<React.SetStateAction<firebase.User | null>>;
@@ -9,11 +9,11 @@ interface LandingPageProps {
 
 const LandingPage: React.FC<LandingPageProps> = ({ setUser }) => {
   return (
-    <div className="min-h-screen bg-gray-100">
+    <div className='min-h-screen'>
       <Navbar setUser={setUser} />
-        <Hero12 />
-      <footer className="bg-gray-800 text-white py-4">
-        <div className="container mx-auto text-center">
+      <Hero12 />
+      <footer className='bg-[#344D59] text-white py-4'>
+        <div className='container mx-auto text-center'>
           <p>&copy; 2023 Your Company. All rights reserved.</p>
         </div>
       </footer>
--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -1,50 +1,55 @@
-import React, { useEffect } from 'react';
-import { auth } from '@/firebase/firebaseConfig';
-import { signInWithPopup, onAuthStateChanged, GithubAuthProvider } from 'firebase/auth';
-import { Dispatch, SetStateAction } from 'react';
-import firebase from 'firebase/compat/app';
-import { useNavigate } from 'react-router-dom';
+import React, { useEffect } from "react";
+import { auth } from "../firebase/firebaseConfig";
+import {
+  signInWithPopup,
+  onAuthStateChanged,
+  GithubAuthProvider,
+} from "firebase/auth";
+import { Dispatch, SetStateAction } from "react";
+import firebase from "firebase/compat/app";
+import { useNavigate } from "react-router-dom";
 
 interface LoginProps {
-    onUserChange: Dispatch<SetStateAction<firebase.User | null>>;
-    loginText: string;
+  onUserChange: Dispatch<SetStateAction<firebase.User | null>>;
+  loginText: string;
 }
 
 const Login: React.FC<LoginProps> = ({ onUserChange, loginText }) => {
-    const navigate = useNavigate();
-    const provider = new GithubAuthProvider(); // Utiliser GitHub comme fournisseur
+  const navigate = useNavigate();
+  const provider = new GithubAuthProvider(); // Utiliser GitHub comme fournisseur
 
-    const handleLogin = async () => {
-        try {
-            const result = await signInWithPopup(auth, provider);
-            console.log(result.user);
-            onUserChange(result.user as firebase.User);
-            navigate('/dashboard');
-        } catch (error) {
-            console.error(error);
-        }
-    };
+  const handleLogin = async () => {
+    try {
+      const result = await signInWithPopup(auth, provider);
+      console.log(result.user);
+      onUserChange(result.user as firebase.User);
+      navigate("/dashboard");
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-    useEffect(() => {
-        const unsubscribe = onAuthStateChanged(auth, (user) => {
-            onUserChange(user as firebase.User);
-            if (user) {
-                navigate('/dashboard');
-            }
-        });
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      onUserChange(user as firebase.User);
+      if (user) {
+        navigate("/dashboard");
+      }
+    });
 
-        return () => unsubscribe();
-    }, [onUserChange, navigate]);
+    return () => unsubscribe();
+  }, [onUserChange, navigate]);
 
-    return (
-        <div className="flex items-center justify-center h-screen">
-            <button
-                className="px-4 py-2 text-white bg-blue-600 rounded"
-                onClick={handleLogin}>
-                {loginText}
-            </button>
-        </div>
-    );
+  return (
+    <div className='flex items-center justify-center h-screen'>
+      <button
+        className='px-4 py-2 text-white bg-[#709CA7] rounded'
+        onClick={handleLogin}
+      >
+        {loginText}
+      </button>
+    </div>
+  );
 };
 
 export default Login;
